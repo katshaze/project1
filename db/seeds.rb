9524c795200fc395a7bf433206c9aad0620f72be
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-03 15:14:08 +1000

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(:username => 'chicken', :email => 'chicken@chicken.com', :password => "chicken")
+u1 = User.create(:username => 'chicken', :email => 'chicken@chicken.com', :password => "chicken")
 User.create(:username => 'katshaze', :email => 'katrinamargaret.hayes@gmail.com', :password => "chicken")
 User.create(:username => 'eastcoastlow', :email => 'rob@rob.com', :password => 'rob')
 
 Book.destroy_all
-Book.create(:title => 'Jane Eyre', :author => 'Charlotte Bronte', :link => 'https://www.goodreads.com/book/show/10210.Jane_Eyre')
+b1 = Book.create(:title => 'Jane Eyre', :author => 'Charlotte Bronte', :link => 'https://www.goodreads.com/book/show/10210.Jane_Eyre')
 Book.create(:title => 'Kafka on the Shore', :author => 'Haruki Murakami', :link => 'https://www.goodreads.com/book/show/4929.Kafka_on_the_Shore?ac=1&from_search=true')
 Book.create(:title => "Alice's Adventures in Wonderland", :author => 'Lewis Carroll', :link => 'https://www.goodreads.com/book/show/6324090-alice-s-adventures-in-wonderland')
+
+Reading.destroy_all
+r1 = Reading.create(:stars => "****")
+
+u1.readings << r1
+b1.readings << r1
