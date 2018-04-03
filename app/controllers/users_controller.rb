8e12a02d207786cfc6740b32503f2542c1bab8ed
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-03 16:30:46 +1000

    gallery code & css slightly refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-    session[:message] = 'HELLO THERE'
+    # session[:message] = 'HELLO THERE'
   end
 
   def create
     @user = User.new user_params # instead of User.create, which immediately tries to save the user into the db, and instead of user it's @user since we need the params to then test it out...(?)
     if @user.save # i.e. if it returns something truthy
-      redirect_to root_path # it worked!
+      redirect_to root_path
+      flash[:success] = "Success! Login to start tracking books."
     else
       render :new
     end
