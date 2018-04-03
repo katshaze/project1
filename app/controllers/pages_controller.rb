e12564921d1ff9243876c6db909cc52fdf0a7cf7
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-03 15:23:24 +1000

    home page current user fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @faves = @current_user.readings.where('readings.stars' => ["****", "*****"])
-    @currently_reading = @current_user.readings.where('readings.reading')
-    @to_read = @current_user.readings.where('readings.to_read')
-    @read = @current_user.readings.where('readings.read')
+    # @users = User.all
+    # @readings = Reading.all
+    if @current_user.present?
+      @faves = @current_user.readings.where('readings.stars' => ["****", "*****"])
+      @currently_reading = @current_user.readings.where('readings.reading')
+      @to_read = @current_user.readings.where('readings.to_read')
+      @read = @current_user.readings.where('readings.read')
+    end
   end
 end
