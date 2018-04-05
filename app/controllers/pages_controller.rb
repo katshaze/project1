707cf832e9289daabc1f48ce7810a73067f42603
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-05 17:01:15 +1000

    shortened titles in empty thumbs donedonedone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def home
-    # raise :hell
     if @current_user.present?
       @faves = @current_user.readings.where('readings.stars' => ["****", "*****"])
       @currently_reading = @current_user.readings.where('readings.reading')
       @to_read = @current_user.readings.where('readings.to_read')
       @read = @current_user.readings.where('readings.read')
     end
+    # @title = @title.split(":").first
   end
 end
