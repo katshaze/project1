1caaaf809808aeea2def9b9b0259f21ad971f5ed
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-05 21:22:05 +1000

    minor tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#home'
-  resources :users, :except => [:index]
+  resources :users
   get '/books/search' => 'books#search'
   post '/books/search' => 'books#import'
   get '/books/result' => 'books#result'
